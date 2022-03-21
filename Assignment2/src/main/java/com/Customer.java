c4ee39bd83da7fb9162df2d519be0dfdd659f9f9
Pravallika Gokeda <79828142+Pravallika52@users.noreply.github.com>
2022-03-21 22:54:47 +0530

    Add files via upload

---
+++ Assignment2/src/main/java/com/Customer.java
@@ -0,0 +1,45 @@
+package com;
+import com.Address;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int customerId;
+	private String customerName;
+	private String customerEmail;
+	private Address customerAddress;
+	public Customer() {
+		
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+	public Address getCustomerAddress() {
+		return customerAddress;
+	}
+	public void setCustomerAddress(Address customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+	
+
+}
---
+++ Assignment3/src/main/java/com/PassengerId.java
@@ -0,0 +1,23 @@
+package com;
+
+
+public class PassengerId{
+	private String passengerEmail;
+	private long passengerMobile;
+	public PassengerId(){
+		
+	}
+	public String getPassengerEmail() {
+		return passengerEmail;
+	}
+	public void setPassengerEmail(String passengerEmail) {
+		this.passengerEmail = passengerEmail;
+	}
+	public long getPassengerMobile() {
+		return passengerMobile;
+	}
+	public void setPassengerMobile(long passengerMobile) {
+		this.passengerMobile = passengerMobile;
+	}
+
+}
